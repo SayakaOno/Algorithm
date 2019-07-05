adad50af784b7a04dfb9e3bf7098dfd09594c512
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-07-04 21:53:08 -0700

    add TwoSum.js

---
+++ LeetCode/TwoSum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (map[nums[i]] || map[nums[i]] === 0) {
+      return [map[nums[i]], i];
+    }
+    map[target - nums[i]] = i;
+  }
+};
