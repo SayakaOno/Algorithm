424dd8eb747d734754b0e169484b118992be1ffa
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-07-14 22:30:21 -0700

    add maximumSubarray.js

---
+++ LeetCode/53_maximumSubarray.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+  // 0(1)
+  let max = nums[0];
+  let sum = max;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > sum + nums[i]) {
+      sum = nums[i];
+    } else {
+      sum += nums[i];
+    }
+    max = Math.max(max, sum);
+  }
+  return max;
+
+  // O(n)
+  // let max = Number.MIN_SAFE_INTEGER;
+  // while (nums.length > 0) {
+  //   let sum = 0;
+  //   for (let i = 0; i < nums.length; i++) {
+  //     sum += nums[i];
+  //     max = Math.max(max, sum);
+  //   }
+  //   nums.shift();
+  // }
+  // return max;
+};
